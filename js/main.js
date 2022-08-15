687fe44f53fff5bc00eb8d7f1ce7fe5f525b2281
withsme <tmdal9953@gmail.com>
2022-08-16 00:47:39 +0900

    Badges completion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// SEARCH
 const searchEl = document.querySelector('.search');
 // document = HTML 그 자체라고 생각
 const searchInputEl = searchEl.querySelector('input');
@@ -20,3 +21,27 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', ''); 
 })
+
+// BADGES
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle(함수, 시간);
+window.addEventListener('scroll', _.throttle(function() {
+  if(window.scrollY > 500) { 
+    // 배지 숨기기
+    // gsap : JS 애니메이션을 처리하는 라이브러리
+    // gsap.to(적용시킬 해당 요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else { 
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle 없이 하면 화면이 스크롤 한 번할 때 몇 십번이나 함수가 실행됨
+// 300 : 0.3s를 의미 -> 일정 시간에 한 번씩만 함수가 실행되도록 하여 여러 번 우르르 실행되는 것을 방지
